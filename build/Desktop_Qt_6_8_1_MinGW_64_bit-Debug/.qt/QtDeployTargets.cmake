39d7053073dc2be26dba92760297c20769fd20be
Gureva Anna Mikhailovna <149677477+anya-ananasss@users.noreply.github.com>
2025-02-02 15:30:54 +0300

    rm redundant files

--- build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_patientsQueue_FILE C:/Users/anya ananasss.HOME-PC/Documents/patientsQueue/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/patientsQueue.exe)
-set(__QT_DEPLOY_TARGET_patientsQueue_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_patientsQueue_RUNTIME_DLLS C:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
